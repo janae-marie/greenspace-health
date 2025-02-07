62ec9e79cae5bc68a40ca3ed348053f5a9075f1e
Janae Thomson <janaethomson@gmail.com>
2025-02-07 09:36:09 -0700

    Load osm data for constant city and ignore macOS files

--- data/osm_loader.py
+++ data/osm_loader.py
@@ -4,8 +4,9 @@ import geopandas
 
 #Load OSM data using place definition
 def load_osm_greenspace():
- greenspace = osmnx.features.features_from_place(\"Phoenix, Arizona\", {'leisure': 'park'}, which_result=None)
+ greenspace = osmnx.features.features_from_place("Phoenix, Arizona", {'leisure': 'park'}, which_result=None)
  return greenspace
- 
-load_osm_greenspace()
-print(greenspace)
+
+greenspace_data = load_osm_greenspace()
+print(greenspace_data.head())
+print(f"Total parks: {len(greenspace_data)}")
