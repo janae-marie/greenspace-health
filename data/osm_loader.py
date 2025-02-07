5c6a09a63800c70e5eaf3be78c438294c5f270ed
Janae Thomson <janaethomson@gmail.com>
2025-02-07 13:51:00 -0700

    Utilize try/excepts for error handling

--- data/osm_loader.py
+++ data/osm_loader.py
@@ -4,20 +4,19 @@ import geopandas
 
 #Load OSM data using place definition and greenspace definition
 def load_osm_greenspace(place_name, tags):
-    greenspace = osmnx.features.features_from_place(place_name, tags, which_result=None)
-    return greenspace
+    try:
+        greenspace = osmnx.features.features_from_place(place_name, tags, which_result=None)
+        
+        #Display greenspace_data basic information
+        #greenspace_data is a GeoDataFrame
+        print(greenspace.head())
+        print(f"Total greenspaces: {len(greenspace)}")
+        print(f"The greenspace data in {place_name} has {len(greenspace.columns)} columns, or attributes, per park")
 
+        return greenspace
+    except:
+        print(f"No greenspaces found for {place_name}")
 
 place_name = 'Phoenix, Arizona'
 greenspace_tags = {'leisure': ['park', 'nature_reserve']}
 greenspace_data = load_osm_greenspace(place_name, greenspace_tags)
-
-#Attempt at error handling but NOT WORKING YET because osmnx doesn't return an empty data set
-if greenspace_data.empty:
-        print(f"No parks found for Phoenix, Arizona")
-else:
-    #Display greenspace_data basic information
-    #greenspace_data is a GeoDataFrame
-    print(greenspace_data.head())
-    print(f"Total greenspace: {len(greenspace_data)}")
-    print(f"{place_name} has {len(greenspace_data.columns)} columns, or attributes, per park")
