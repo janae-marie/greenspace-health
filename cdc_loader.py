a9fd3e6e5f334afcb5cc30dcaf36c3a75292b7e8
Janae Thomson <janaethomson@gmail.com>
2025-02-23 13:31:00 -0700

    Stop using utils cdc_loader.py
    Stop using utils file

--- cdc_loader.py
+++ cdc_loader.py
@@ -2,34 +2,7 @@
 import requests
 import pandas
 import geopandas
-from pull_data_utils import load_data_from_disk
-from pull_data_utils import state_name_to_abbr
 
-
-def load_cdc_places(county_name, state_abbr, measure_id='DEPRESSION'):
-    print("State:", state_abbr, "County:", county_name, "Depression Score:", measure_id)
-    
-    
-def main():
-    data = load_data_from_disk()
-    if data is None:
-        print("[ERROR] Could not load data from disk")
-        return
-
-    # only log LIMIT counties per state
-    LIMIT = 5
-    for state, counties in iter(data.items()):
-        for i, county in enumerate(counties):
-            load_cdc_places(county, state_name_to_abbr(state), measure_id)
-            if i > LIMIT:
-                break
-    return
-
-
-if __name__ == '__main__':
-    main()
-
-'''
 #Load PLACES data using county/state definition & Measure ID (default: depression)
 def load_cdc_places(county_name, state_abbr, measure_id='DEPRESSION', limit=5000):
     try:
@@ -63,4 +36,3 @@ def load_cdc_places(county_name, state_abbr, measure_id='DEPRESSION', limit=5000
 county_name = 'Maricopa'
 state_abbr = 'AZ'
 cdc_data = load_cdc_places(county_name, state_abbr)
-'''
