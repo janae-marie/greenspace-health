12f60b1b8af1bb24f06453414464dd2c2d544504
Heather Tottingham <196674665+hott2tott@users.noreply.github.com>
2025-03-02 20:59:32 -0700

    Added manual values to calculate correlation for three counties

--- data_analysis.py
+++ data_analysis.py
@@ -9,25 +9,25 @@ place_name = 'St. Louis County, Missouri'
 greenspace_tags = {'leisure': ['park', 'nature_reserve', 'garden']}
 osm_data_for_analysis = main_load_greenspace(place_name, greenspace_tags)
 print(osm_data_for_analysis)
+# Run again for 'Laramie County, Wyoming'
 
 # Load CDC data
 state_abbr = 'MO'
 county_name = 'St. Louis'
-cdc_data_for_analysis = load_cdc_data(county_name, state_abbr)
+cdc_data_for_analysis = load_cdc_data(county_name, state_abbr, measure_id)
 print(cdc_data_for_analysis)
+# Run again for 'Laramie, WY'
 
 
 # Need to convert greenspace data to a score to correlate with CDC Depression score
 
 # Correlation Analysis
-x = numpy.array([1, 2, 3, 4, 5])
-y = numpy.array([2, 4, 5, 4, 5])
-correlation_coefficient = numpy.corrcoef(x, y)[0, 1]
-print(correlation_coefficient)
-# 0.7745966692414834
+depression_values = numpy.array([24.1, 22.1, 20.5])
+# Depression values for St. Louis County MO, Laramie County WY, and Polk County IA
+greenspace_values = numpy.array([652, 127, 4288])
+# Greenspace counts for St. Louis County MO, Laramie County WY, and Polk County IA
 
-# So, for our correlation analysis, we need arrays of equal size
-# Data Matrix: County, State, Depression_Value, Greenspace_Value
-# Calculate numpy.corrcoef(Depression_Value, Greenspace_Value)[0, 1]
-correlation_coefficient = numpy.corrcoef(Data_Value, leisure_counts)
+correlation_coefficient = numpy.corrcoef(depression_values, greenspace_values)[0, 1]
 print(correlation_coefficient)
+# -0.7624134017970408
+
